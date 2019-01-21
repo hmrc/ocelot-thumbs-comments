b8f958d5eae3bb617008479d501d013b30948c64
Paul Temperley <paul.temperley@digital.hmrc.gov.uk>
2019-01-21 11:36:40 +0000

    inital commit

--- ThumbsComments/Startup.cs
+++ ThumbsComments/Startup.cs
@@ -25,7 +25,10 @@ namespace ThumbsComments
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<Context>(options =>
+               options
+                   .UseSqlServer(Configuration.GetConnectionString("DefaultConnection") +
+                           Environment.GetEnvironmentVariable("Connection", EnvironmentVariableTarget.Machine)));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
