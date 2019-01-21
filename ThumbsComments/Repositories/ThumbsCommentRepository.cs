61863f2a1eba9e0def0b56e4a7a5bff9955cc401
Paul Temperley <paul.temperley@digital.hmrc.gov.uk>
2019-01-21 14:30:18 +0000

    ThumbsCommentRepository created

---
+++ ThumbsComments/Repositories/ThumbsCommentRepository.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using ThumbsComments.Interfaces;
+using ThumbsComments.Models;
+
+namespace ThumbsComments.Repositories
+{
+    public class ThumbsCommentRepository : IThumbsCommentsRepository
+    {
+        private readonly Context _context;
+
+        public ThumbsCommentRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<Comment> Get(Expression<Func<Comment, bool>> where)
+        {
+            return await _context.Comments
+                                 .Where(where)
+                                 .FirstOrDefaultAsync();
+                                        
+        }
+
+        public async Task<IEnumerable<Comment>> GetMany()
+        {
+            return await _context.Comments
+                                 .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Comment>> GetMany(Expression<Func<Comment, bool>> where)
+        {
+            return await _context.Comments
+                                 .Where(where)
+                                 .ToListAsync();
+        }
+
+        public async Task Post(Comment comment)
+        {
+            _context.Add(comment);
+            await _context.SaveChangesAsync();
+            return;
+        }
+
+        public async Task Put(Comment comment)
+        {
+            _context.Update(comment);
+            await _context.SaveChangesAsync();
+            return;
+        }
+
+        public async Task Delete(Comment comment)
+        {
+            _context.Comments
+                    .Remove(comment);
+
+            await _context.SaveChangesAsync();
+
+            return;
+        }
+
+        public async Task<bool> Exists(Guid id)
+        {
+            return await _context.Comments
+                                 .AnyAsync();
+        }
+    }
+}
